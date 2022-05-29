a4c363001677786974585a42c4ae77a5d5b622ab
Kushagra <kushagra.acharya@outlook.com>
2022-05-30 00:07:51 +0545

    copy paste d3

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,36 @@
+export const navLinks = [
+  // {
+  //   label: 'Projects',
+  //   pathname: '/#project-1',
+  // },
+  // {
+  //   label: 'Details',
+  //   pathname: '/#details',
+  // },
+  // {
+  //   label: 'Articles',
+  //   pathname: '/articles',
+  // },
+  // {
+  //   label: 'Contact',
+  //   pathname: '/contact',
+  // },
+];
+
+export const socialLinks = [
+  // {
+  //   label: 'Facebook',
+  //   url: 'https://twitter.com/hamishMW',
+  //   icon: 'twitter',
+  // },
+  {
+    label: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/kushagraacharya/',
+    icon: 'figma',
+  },
+  {
+    label: 'Github',
+    url: 'https://github.com/schixor88',
+    icon: 'github',
+  },
+];
